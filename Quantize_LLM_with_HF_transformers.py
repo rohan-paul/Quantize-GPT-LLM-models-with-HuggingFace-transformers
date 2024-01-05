ce29d6905ae48b639c85338fca4366f054f17a04
Rohan Paul <rohanpaul2@gmail.com>
2024-01-05 16:55:49 +0530

    "updating"

--- Quantize_LLM_with_HF_transformers.py
+++ Quantize_LLM_with_HF_transformers.py
@@ -9,6 +9,7 @@ def get_device(device_map):
     return device_map
 
 # Function to configure and return the quantized model
+# https://huggingface.co/docs/transformers/main_classes/quantization#transformers.GPTQConfig
 def configure_model(model_id, bits, dataset, tokenizer, group_size, device):
     gptq_config = GPTQConfig(bits=bits, dataset=dataset, tokenizer=tokenizer, group_size=group_size, desc_act=True)
     model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=gptq_config)
@@ -27,11 +28,13 @@ def main(model_id, bits, dataset, group_size, device_map):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Quantize a GPT model.")
-    parser.add_argument("--model_id", default="teknium/OpenHermes-2-Mistral-7B", type=str, help="The pretrained model ID.")
+    parser.add_argument("--model_id", default="mistralai/Mistral-7B-v0.1", type=str, help="The pretrained model ID.")
     parser.add_argument("--bits", default=4, type=int, help="Number of bits for quantization.")
     parser.add_argument("--dataset", default="wikitext2", type=str, help="The dataset to use.")
     parser.add_argument("--group_size", default=128, type=int, help="Group size for quantization.")
     parser.add_argument("--device_map", default="auto", type=str, help="Device map for loading the model.")
+    parser.add_argument("--use_exllama", default="True", type=bool, help="Whether to use exllama backend. Defaults to True if unset. Only works with bits = 4.")
+    parser.add_argument("--desc_act", default="False", type=bool, help=" Whether to quantize columns in order of decreasing activation size. Setting it to False can significantly speed up inference but the perplexity may become slightly worse. Also known as act-order.")
 
     args = parser.parse_args()
 
---
+++ README.md
@@ -0,0 +1,2 @@
+For all params of GPTQConfig check its doc
+https://huggingface.co/docs/transformers/main_classes/quantization#transformers.GPTQConfig
